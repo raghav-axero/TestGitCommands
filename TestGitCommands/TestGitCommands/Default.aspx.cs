0b20cc5c7bb7fd22cd72c4f40e9494f62b1a671c
raghav-khunger <raghav.khunger@gmail.com>
2014-03-25 14:07:37 +0530

    Fix: System.Web.UI namespace added

--- TestGitCommands/TestGitCommands/Default.aspx.cs
+++ TestGitCommands/TestGitCommands/Default.aspx.cs
@@ -1,8 +1,9 @@
 ﻿using System;
+using System.Web.UI;
 
 namespace TestGitCommands
 {
-    public partial class _Default : System.Web.UI.Page
+    public partial class _Default : Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
